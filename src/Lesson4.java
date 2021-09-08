db555729e61c0a1105414032831b592ce7416525
Zhirayr2000 <zhirayrtovmasyan2000@gmail.com>
2021-09-08 20:11:40 +0400

    fds

--- src/Lesson4.java
+++ src/Lesson4.java
@@ -1,5 +1,23 @@
 public class Lesson4 {
     public static void main(String[] args) {
-        
+        int a=10;
+        while(a!=0) {
+            if (a % 2 == 0)
+                System.out.print(a + " ");
+            a--;
+        }
+        System.out.println();
+        do{
+            if (a % 2 ==-1)
+                System.out.print(a + " ");
+            a--;
+        }while(a!=-10);
+        System.out.println();
+        for (int i = 1; ; i++) {
+            if(i%2==0) continue;
+            System.out.print(i+" ");
+            if(i>10)
+                break;
+        }
     }
 }
